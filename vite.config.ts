12b5a3b420282e6dad35371d9c5ac3b12b076665
Justin Winoto <winoto.justin@gmail.com>
2025-02-28 07:12:11 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Trial--3/',
+  base: '/Trial4/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
